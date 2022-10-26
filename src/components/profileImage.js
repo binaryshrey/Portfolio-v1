a5e87187125d30dcb270d2e5be6c62ea464fa770
binaryshrey <shreyansh.saurabh2016@vitstudent.ac.in>
2022-10-26 12:31:21 +0530

    updates folder str

---
+++ src/components/profileImage.js
@@ -0,0 +1,8 @@
+import React from "react"
+import * as styles from "../../src/Styles.module.css"
+
+const ProfileImage = () => {
+  return <></>
+}
+
+export default ProfileImage
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,10 @@
 import React from "react"
+import ProfileImage from "../components/profileImage"
 
 export default function Home() {
-  return <div>Hello world!</div>
+  return (
+    <div>
+      <ProfileImage />
+    </div>
+  )
 }
