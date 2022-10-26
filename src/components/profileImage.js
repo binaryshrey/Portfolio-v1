cf15c9a98972ee000aa8a2ee67580170bc030c66
binaryshrey <shreyansh.saurabh2016@vitstudent.ac.in>
2022-10-26 12:41:48 +0530

    updates profileImage

--- src/components/profileImage.js
+++ src/components/profileImage.js
@@ -1,8 +1,19 @@
 import React from "react"
 import * as styles from "../../src/Styles.module.css"
+import DB from "../utils/db.json"
 
 const ProfileImage = () => {
-  return <></>
+  return (
+    <>
+      <img
+        src={DB.profileImage}
+        alt="profile-pic"
+        width="120"
+        height="120"
+        className={styles.profilePic}
+      />
+    </>
+  )
 }
 
 export default ProfileImage
