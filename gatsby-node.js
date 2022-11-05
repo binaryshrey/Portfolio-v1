2df37d1e0ad7a37d0164ad34d20c7ef9c7a3255a
binaryshrey <shreyansh.saurabh2016@vitstudent.ac.in>
2022-11-05 16:31:49 +0530

    updates resume pdf viewer functionality

--- gatsby-node.js
+++ gatsby-node.js
@@ -5,13 +5,25 @@ exports.onCreateWebpackConfig = ({
   plugins,
   actions,
 }) => {
-  if (stage === "build-html") {
+  if (stage === "build-html" || stage === "develop-html") {
     actions.setWebpackConfig({
       module: {
         rules: [
           {
             test: /canvas/,
             use: loaders.null(),
+          },
+
+          {
+            test: /\.(png|svg|jpg|gif|pdf)$/,
+            use: [
+              {
+                loader: "file-loader",
+                options: {
+                  name: "[name].[ext]",
+                },
+              },
+            ],
           },
         ],
       },
--- src/pages/resume.jsx
+++ src/pages/resume.jsx
@@ -1,7 +1,28 @@
 import React, { useState } from "react"
+import PDF from "../../static/Resume.pdf"
+import "@react-pdf-viewer/core/lib/styles/index.css"
+import { Worker, Viewer } from "@react-pdf-viewer/core"
+import "@react-pdf-viewer/default-layout/lib/styles/index.css"
+import { defaultLayoutPlugin } from "@react-pdf-viewer/default-layout"
 
 const Resume = () => {
-  return <></>
+  const [resumePDF] = useState(PDF)
+  const defaultLayoutPluginInstance = defaultLayoutPlugin()
+
+  return (
+    <>
+      {resumePDF && (
+        <>
+          <Worker workerUrl="https://unpkg.com/pdfjs-dist@2.16.105/build/pdf.worker.min.js">
+            <Viewer
+              fileUrl={resumePDF}
+              plugins={[defaultLayoutPluginInstance]}
+            />
+          </Worker>
+        </>
+      )}
+    </>
+  )
 }
 
 export default Resume
