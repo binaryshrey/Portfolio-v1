ff408ebc3bd8395fdf51e2bfa183292469ac3710
binaryshrey <shreyansh.saurabh2016@vitstudent.ac.in>
2023-07-24 13:19:40 +0530

    updates deps

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,17 +1,30 @@
 module.exports = {
-  /* Your site config here */
-  plugins: [
-    `gatsby-transformer-json`,
-    {
-      resolve: `gatsby-source-filesystem`,
+  plugins: [{
+      resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `data`,
-        path: `${__dirname}/src/data/`,
+        name: 'Shreyansh Saurabh',
+        short_name: 'Shreyansh Saurabh',
+        start_url: '/',
+        background_color: '#000000',
+        theme_color: '#ffffff',
+        display: 'standalone',
+        icon: './logo.svg',
       },
     },
+    {
+      resolve: 'gatsby-plugin-html-attributes',
+      options: {
+        lang: 'en'
+      }
+    },
+    {
+      resolve: 'gatsby-plugin-netlify',
+    }
   ],
   siteMetadata: {
     title: "Shreyansh Saurabh",
     description: "FullStack Android Developer : Portfolio site",
+    image: `/logo.webp`,
+    siteUrl: `https://shreyansh.dev/`,
   },
 }
