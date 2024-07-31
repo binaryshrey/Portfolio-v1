7c84e532d3e67333dbe36f1c56544092af943dcd
binaryshrey <shreyansh.saurabh2016@vitstudent.ac.in>
2024-07-31 23:21:31 +0530

    updates db data

--- src/components/aboutContent.jsx
+++ src/components/aboutContent.jsx
@@ -1,43 +1,59 @@
-import * as React from "react"
-import Card from "@mui/material/Card"
-import darkTheme from "../utils/appTheme"
+import * as React from "react";
+import Card from "@mui/material/Card";
+import darkTheme from "../utils/appTheme";
 import db from "../data/db.json";
-import { useMediaQuery } from "react-responsive"
-import Typography from "@mui/material/Typography"
-import CardContent from "@mui/material/CardContent"
+import { useMediaQuery } from "react-responsive";
+import Typography from "@mui/material/Typography";
+import CardContent from "@mui/material/CardContent";
 
 const AboutContent = () => {
-  const isDesktop = useMediaQuery({
-    query: "(min-width: 769px)",
-  })
+	const isDesktop = useMediaQuery({
+		query: "(min-width: 769px)",
+	});
 
-  return (
-    <Card
-      style={{
-        background: darkTheme.palette.action.disabledBackground,
-        margin: isDesktop ? "3rem 3rem 1.5rem" : "3rem 0.75rem 1.5rem",
-        borderRadius: "0.75rem",
-      }}
-    >
-      <CardContent>
-        <Typography variant="body1" style={{ whiteSpace: "pre-line" }}>
-          {db.description1}
-        </Typography>
+	return (
+		<Card
+			style={{
+				background: darkTheme.palette.action.disabledBackground,
+				margin: isDesktop ? "3rem 3rem 1.5rem" : "3rem 0.75rem 1.5rem",
+				borderRadius: "0.75rem",
+			}}
+		>
+			<CardContent style={{ margin: "8px" }}>
+				<Typography
+					variant="h6"
+					style={{ whiteSpace: "pre-line", paddingBottom: "12px" }}
+				>
+					Hey, this is Shreyansh :)
+				</Typography>
 
-        <Typography variant="body1" style={{ whiteSpace: "pre-line", marginTop:'2rem'}}>
-          {db.description2}
-        </Typography>
+				<Typography variant="body1" style={{ whiteSpace: "pre-line" }}>
+					{db.description1}
+				</Typography>
 
-        <Typography variant="body1" style={{ whiteSpace: "pre-line", marginTop:'2rem'}}>
-          {db.description3}
-        </Typography>
+				<Typography
+					variant="body1"
+					style={{ whiteSpace: "pre-line", marginTop: "2rem" }}
+				>
+					{db.description2}
+				</Typography>
 
-        <Typography variant="body1" style={{ whiteSpace: "pre-line", marginTop:'2rem'}}>
-          {db.description4}
-        </Typography>
-      </CardContent>
-    </Card>
-  )
-}
+				<Typography
+					variant="body1"
+					style={{ whiteSpace: "pre-line", marginTop: "2rem" }}
+				>
+					{db.description3}
+				</Typography>
 
-export default AboutContent
+				<Typography
+					variant="body1"
+					style={{ whiteSpace: "pre-line", marginTop: "2rem" }}
+				>
+					{db.description4}
+				</Typography>
+			</CardContent>
+		</Card>
+	);
+};
+
+export default AboutContent;
